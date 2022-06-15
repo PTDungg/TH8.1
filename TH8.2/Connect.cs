deb416109e2ee356f47a9076615828f3af0c647f
Phạm Thế Dũng <95071172+PTDungg@users.noreply.github.com>
2022-06-15 10:20:31 +0700

    Add project files.

---
+++ TH8.2/Connect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TH8._2
+{
+    internal class Connect
+    {
+        public static string strSql = "Data Source=HUNG;Initial Catalog=qli_sv;Integrated Security=True";
+
+        public static SqlConnection getCon()
+        {
+            return new SqlConnection(strSql);
+        }
+    }  
+}
---
+++ qli_sv.sql
@@ -0,0 +1,12 @@
+﻿create database qli_sv
+use qli_sv
+
+create table SINHVIEN
+(
+	ma_sv varchar(50) primary key not null,
+	ten_sv nvarchar(50),
+	ngay_sinh date,
+	gioi_tinh nvarchar(50),
+	dia_chi nvarchar(50),
+)
+select * from SINHVIEN where ma_sv like '1' or ten_sv like N'Hưng' or gioi_tinh like 'Nam'
